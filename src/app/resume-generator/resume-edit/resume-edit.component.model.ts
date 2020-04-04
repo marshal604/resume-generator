4f5374678a31d30f083ef34c5303d546583600bd
Lerdor <marshal604@gmail.com>
2020-04-04 14:13:13 +0800

    fix(ResumeEditComponent): fetch up losing model

--- src/app/resume-generator/resume-edit/resume-edit.component.model.ts
+++ src/app/resume-generator/resume-edit/resume-edit.component.model.ts
@@ -1,5 +1,17 @@
 import { ResumeEditAboutMeFormData } from './resume-edit-about-me/resume-edit-about-me.component.model';
+import { ResumeEditEducationFormData } from './resume-edit-education/resume-edit-education.component.model';
+import { ResumeEditProfileFormData } from './resume-edit-profile/resume-edit-profile.component.model';
+import { ResumeEditExperienceFormData } from './resume-edit-experience/resume-edit-experience.component.model';
 
+export enum ResumeEditFormProperties {
+  AboutMe = 'aboutMe',
+  Education = 'education',
+  Profile = 'profile',
+  Experience = 'experience'
+}
 export interface ResumeEditFormData {
-  aboutMe: ResumeEditAboutMeFormData;
+  [ResumeEditFormProperties.AboutMe]: ResumeEditAboutMeFormData;
+  [ResumeEditFormProperties.Education]: ResumeEditEducationFormData;
+  [ResumeEditFormProperties.Profile]: ResumeEditProfileFormData;
+  [ResumeEditFormProperties.Experience]: ResumeEditExperienceFormData;
 }
